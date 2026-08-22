1386b6a9094d7a851bcdcf6a67dbef77ad33e629
Meecka <76573720+Meecka@users.noreply.github.com>
2026-08-21 20:43:59 -0500

    Submodule fix

--- STM32CubeIDE/Debug/Application/User/LWIP/App/udp_echoclient.d
+++
@@ -1,2 +0,0 @@
-Application/User/LWIP/App/udp_echoclient.o: \
- C:/Users/fanta/OneDrive/Documents/GitHub/stm32h5-classic-coremw-apps/Projects/NUCLEO-H563ZI/Applications/LwIP/LwIP_UDP_Echo_Client/LWIP/App/udp_echoclient.c
--- STM32CubeIDE/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- C:/Users/fanta/OneDrive/Documents/GitHub/stm32h5-classic-coremw-apps/Drivers/BSP/Components/lan8742/lan8742.c \
- C:/Users/fanta/OneDrive/Documents/GitHub/stm32h5-classic-coremw-apps/Drivers/BSP/Components/lan8742/lan8742.h
-C:/Users/fanta/OneDrive/Documents/GitHub/stm32h5-classic-coremw-apps/Drivers/BSP/Components/lan8742/lan8742.h:
